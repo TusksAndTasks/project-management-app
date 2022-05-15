b592fc7979692869f8904ad623d19d67deffa510
ovasilena <olgavasilena@gmail.com>
2022-05-15 14:03:03 +0200

    feat: add ant layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from '../header/header';
+import { Layout } from 'antd';
+import NavHeader from '../header/header';
 import Main from '../../feature/main/main';
 import Profile from '../../feature/profile/profile';
 import LogIn from '../../feature/logIn/logIn';
@@ -8,19 +9,26 @@ import DesignComponents from '../designComponents/designComponents';
 import PageNotFound from '../pageNotFound/pageNotFound';
 import './app.scss';
 
+const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/LogIn" element={<LogIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/demo" element={<DesignComponents />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </div>
+    <Layout className="app-wrapper">
+      <Header>
+        <NavHeader />
+      </Header>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/demo" element={<DesignComponents />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Content>
+      <Footer>@Team44</Footer>
+    </Layout>
   );
 }
 
