8637bd70db16095fc771cec3117d251eaa4b5eae
ovasilena <olgavasilena@gmail.com>
2022-05-15 23:50:12 +0200

    feat: add icons, styles

--- src/components/navHeader/navHeader.tsx
+++ src/components/navHeader/navHeader.tsx
@@ -2,7 +2,18 @@ import './navHeader.scss';
 import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 import Search from 'antd/lib/input/Search';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import {
+  FileDoneOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  ScheduleOutlined,
+  SettingOutlined,
+  TeamOutlined,
+  UsergroupAddOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { LanguageEnum } from '../../redux/slices/localization/localizationTypes';
 import { locales } from './locales';
 import { useLocales } from '../../helpers/hooks/useLocales';
@@ -11,25 +22,23 @@ export default function NavHeader() {
   const [language, setLang] = useLocales();
   const [isLogged, setIsLogged] = useState<boolean>(true);
 
-  useEffect(() => {}, [isLogged]);
-
   return (
     <>
       <div className="logo" />
-      <Menu defaultSelectedKeys={['/']} mode="horizontal" className="nav-left">
-        <Menu.Item key="/">
+      <Menu defaultSelectedKeys={['main']} mode="horizontal" className="nav-left">
+        <Menu.Item key="main" icon={<HomeOutlined />}>
           {locales[language].mainLink}
           <Link to="/" />
         </Menu.Item>
-        <Menu.Item key="/Boards">
+        <Menu.Item key="boards" icon={<ScheduleOutlined />}>
           {locales[language].boards}
           <Link to="/Boards" />
         </Menu.Item>
-        <Menu.Item key="/Tasks">
+        <Menu.Item key="tasks" icon={<FileDoneOutlined />}>
           {locales[language].tasks}
           <Link to="/Tasks" />
         </Menu.Item>
-        <Menu.Item key="/People">
+        <Menu.Item key="people" icon={<TeamOutlined />}>
           {locales[language].people}
           <Link to="/People" />
         </Menu.Item>
@@ -37,6 +46,7 @@ export default function NavHeader() {
       <Search />
       <Menu mode="horizontal" className="nav-right">
         <Menu.Item
+          key="language"
           onClick={() => {
             if (language === LanguageEnum.ENG) {
               setLang(LanguageEnum.RUS);
@@ -45,29 +55,40 @@ export default function NavHeader() {
             }
           }}
         >
-          Change language
+          {language === LanguageEnum.ENG ? 'EN' : 'RU'}
         </Menu.Item>
+        {isLogged && (
+          <Menu.Item key="profile" icon={<SettingOutlined />}>
+            {locales[language].profileLink}
+            <Link to="/Profile" />
+          </Menu.Item>
+        )}
         {isLogged ? (
           <Menu.Item
-            key="/"
+            key="home"
+            icon={<LogoutOutlined />}
             onClick={() => {
               setIsLogged(false);
             }}
           >
-            LogOut
             <Link to="/" />
           </Menu.Item>
         ) : (
-          <Menu.Item key="/LogIn">
-            {locales[language].logInLink}
-            <Link to="/LogIn" />
-          </Menu.Item>
-        )}
-        {!isLogged && (
-          <Menu.Item key="/SignUp">
-            {locales[language].signUpLink}
-            <Link to="/SignUp" />
-          </Menu.Item>
+          <Menu.SubMenu icon={<LoginOutlined />} key="SubMenu">
+            <Menu.Item
+              key="logIn"
+              icon={<UserOutlined />}
+              onClick={() => {
+                setIsLogged(true);
+              }}
+            >
+              {locales[language].logInLink}
+            </Menu.Item>
+            <Menu.Item key="signUp" icon={<UsergroupAddOutlined />}>
+              {locales[language].signUpLink}
+              <Link to="/SignUp" />
+            </Menu.Item>
+          </Menu.SubMenu>
         )}
       </Menu>
     </>
