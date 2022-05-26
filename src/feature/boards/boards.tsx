81170f5c6bb0e102f72ca0773d1c1ac704b014d3
ovasilena <olgavasilena@gmail.com>
2022-05-26 21:34:43 +0200

    feat: add styles for boards list

--- src/feature/boards/boards.tsx
+++ src/feature/boards/boards.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { List } from 'antd';
-import { FileImageOutlined, SnippetsOutlined } from '@ant-design/icons';
+import { List, Modal } from 'antd';
+import { FileImageOutlined, PlusCircleFilled, SnippetsOutlined } from '@ant-design/icons';
 import { useBoardsList } from '../../helpers/hooks/useBoardsList';
 import { useAuthToken } from '../../helpers/hooks/useAuthToken';
 import { useLocales } from '../../helpers/hooks/useLocales';
@@ -9,13 +9,22 @@ import { useBoardData } from '../../helpers/hooks/useBoardData';
 import { useColumnList } from '../../helpers/hooks/useColumnList';
 import IconText from './iconText';
 import { locales } from '../main/locales';
+import './boards.scss';
+import { BoardCreatorForm } from '../../components/boardCreatorForm.tsx/boardCreatorForm';
 
 export default function Boards() {
-  const [boardsData, getBoardsList, , deleteBoard] = useBoardsList();
+  const [boardsData, getBoardsList, createBoard, deleteBoard] = useBoardsList();
   const [, getColumnsList] = useColumnList();
   const [, showBoard] = useBoardData();
   const [authToken] = useAuthToken();
   const [language] = useLocales();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleClose = () => {
+    setIsModalVisible(false);
+  };
 
   useEffect(() => {
     getBoardsList(authToken);
@@ -27,41 +36,70 @@ export default function Boards() {
   }
 
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      pagination={{
-        onChange: (page) => {
-          console.log(page);
-        },
-        pageSize: 3,
-      }}
-      dataSource={boardsData.boards}
-      renderItem={(item) => (
-        <List.Item
-          key={item.id}
-          actions={[
-            <IconText icon={SnippetsOutlined} text="10" key="list-vertical-star-o" />,
-            <IconText icon={FileImageOutlined} text="2" key="list-vertical-message" />,
-          ]}
-        >
-          <List.Item.Meta
-            title={(
-              <Link to="/Board" id={item.id} onClick={(e) => getBoardsData(e)}>
-                {item.title}
-              </Link>
+    <>
+      <button className="btn btn-create" type="button" onClick={showModal}>
+        <PlusCircleFilled />
+        {locales[language].createButton}
+      </button>
+      <Modal
+        title={locales[language].creatorTitle}
+        visible={isModalVisible}
+        onCancel={handleClose}
+        footer={[]}
+      >
+        <BoardCreatorForm createBoard={createBoard} authToken={authToken} handleOk={handleClose} />
+      </Modal>
+
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={boardsData.boards}
+        renderItem={(item) => (
+          <List.Item
+            key={item.id}
+            actions={[
+              <IconText icon={SnippetsOutlined} text="10" key="list-vertical-star-o" />,
+              <IconText icon={FileImageOutlined} text="2" key="list-vertical-message" />,
+            ]}
+            extra={(
+              <>
+                <Link
+                  className="btn btn-primary"
+                  to="/Board"
+                  type="button"
+                  id={item.id}
+                  onClick={(e) => getBoardsData(e)}
+                >
+                  {locales[language].showBoard}
+                </Link>
+                <button
+                  className="btn btn-primary btn-delete"
+                  type="button"
+                  id={item.id}
+                  onClick={(e) => deleteBoard({ id: (e.target as HTMLElement).id, token: authToken })}
+                >
+                  {locales[language].deleteButton}
+                </button>
+              </>
             )}
-            description={item.description}
-          />
-          <button
-            type="button"
-            id={item.id}
-            onClick={(e) => deleteBoard({ id: (e.target as HTMLElement).id, token: authToken })}
           >
-            {locales[language].deleteButton}
-          </button>
-        </List.Item>
-      )}
-    />
+            <List.Item.Meta
+              title={(
+                <Link to="/Board" id={item.id} onClick={(e) => getBoardsData(e)}>
+                  {item.title}
+                </Link>
+              )}
+              description={item.description}
+            />
+          </List.Item>
+        )}
+      />
+    </>
   );
 }
