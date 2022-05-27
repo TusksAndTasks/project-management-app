50877469c9f580c3f83d59b65019c14e69db1c48
ovasilena <olgavasilena@gmail.com>
2022-05-27 22:43:40 +0200

    feat: add component profile

--- src/feature/profile/profile.tsx
+++ src/feature/profile/profile.tsx
@@ -1,8 +1,64 @@
-import { locales } from './locales';
-import { useLocales } from '../../helpers/hooks/useLocales';
+import React, { useEffect } from 'react';
+import { Button, Form, Input, Typography } from 'antd';
+import { useUserData } from '../../helpers/hooks/useUserData';
+import { useAuthToken } from '../../helpers/hooks/useAuthToken';
+import './profile.scss';
 
 export default function Profile() {
-  const [language] = useLocales();
+  const [authToken] = useAuthToken();
+  const [usersData, getUsersList] = useUserData();
 
-  return <div>{locales[language].title}</div>;
+  useEffect(() => {
+    getUsersList(authToken);
+  }, []);
+
+  return usersData.loading ? (
+    <div>Load..</div>
+  ) : (
+    <div>
+      {usersData.users
+        .filter((item) => item.login === localStorage.getItem('user'))
+        .map((user) => (
+          <Form
+            name="updateUserForm"
+            initialValues={{ remember: true }}
+            autoComplete="off"
+            labelAlign="left"
+            wrapperCol={{ span: 24 }}
+            labelCol={{ span: 24 }}
+            fields={[
+              {
+                name: ['Login'],
+                value: user.login,
+              },
+              {
+                name: ['Name'],
+                value: user.name,
+              },
+              {
+                name: ['Password'],
+                value: '',
+              },
+            ]}
+          >
+            <Form.Item name="Login" label="Login" className="form-label">
+              <Typography>
+                <pre>{user.login}</pre>
+              </Typography>
+            </Form.Item>
+            <Form.Item name="Name" label="Name" className="form-label">
+              <Input />
+            </Form.Item>
+            <Form.Item name="Password" label="Password" className="form-label">
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 11 }}>
+              <Button type="primary" className="btn btn-primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        ))}
+    </div>
+  );
 }
